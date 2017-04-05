d1070404893fb24580c61282f73b8a29296c4a03
AryBin <arybin@163.com>
2017-04-05 22:55:58 +0800

    fixed "synchronization" problem completely

--- library/src/main/java/com/truizlop/fabreveallayout/FABRevealLayout.java
+++ library/src/main/java/com/truizlop/fabreveallayout/FABRevealLayout.java
@@ -36,291 +36,304 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class FABRevealLayout extends RelativeLayout {
 
-  private static final int MAX_CHILD_VIEWS = 2;
-  private static final int FAB_SIZE = 48;
-  private static final int ANIMATION_DURATION = 500;
-  private final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
+    private static final int MAX_CHILD_VIEWS = 2;
+    private static final int FAB_SIZE = 48;
+    private static final int ANIMATION_DURATION = 500;
+    private final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
 
-  private AtomicBoolean isAnimating = new AtomicBoolean(false);
-  private List<View> childViews = null;
-  private FloatingActionButton fab = null;
-  private CircularExpandingView circularExpandingView = null;
-  private OnRevealChangeListener onRevealChangeListener = null;
-  private OnClickListener fabClickListener = new OnClickListener() {
+    private AtomicBoolean isAnimating = new AtomicBoolean(false);
+    private List<View> childViews = null;
+    private FloatingActionButton fab = null;
+    private CircularExpandingView circularExpandingView = null;
+    private OnRevealChangeListener onRevealChangeListener = null;
+    private OnClickListener fabClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            revealSecondaryView();
+        }
+    };
+
+    public FABRevealLayout(Context context) {
+        this(context, null);
+    }
+
+    public FABRevealLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FABRevealLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        childViews = new ArrayList<>(2);
+    }
+
     @Override
-    public void onClick(View v) {
-      revealSecondaryView();
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        setupView(child);
+        super.addView(child, index, params);
+
+        if (areAllComponentsReady()) {
+            setupInitialState();
+        }
     }
-  };
 
-  public FABRevealLayout(Context context) {
-    this(context, null);
-  }
-
-  public FABRevealLayout(Context context, AttributeSet attrs) {
-    this(context, attrs, 0);
-  }
-
-  public FABRevealLayout(Context context, AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-    childViews = new ArrayList<>(2);
-  }
-
-  @Override
-  public void addView(View child, int index, ViewGroup.LayoutParams params) {
-    setupView(child);
-    super.addView(child, index, params);
-
-    if(areAllComponentsReady()){
-      setupInitialState();
+    private void setupView(View child) {
+        if (child instanceof FloatingActionButton) {
+            setupFAB(child);
+        } else if (!(child instanceof CircularExpandingView)) {
+            setupChildView(child);
+        }
     }
-  }
 
-  private void setupView(View child) {
-    if(child instanceof FloatingActionButton){
-      setupFAB(child);
-    }else if(!(child instanceof CircularExpandingView)){
-      setupChildView(child);
+    private void setupFAB(View view) {
+        validateFAB();
+        fab = (FloatingActionButton) view;
+        fab.setOnClickListener(fabClickListener);
     }
-  }
 
-  private void setupFAB(View view){
-    validateFAB();
-    fab = (FloatingActionButton) view;
-    fab.setOnClickListener(fabClickListener);
-  }
-
-  private void setupChildView(View view){
-    validateChildView();
-    childViews.add(view);
-    if(childViews.size() == 1){
-      addCircularRevealView();
+    private void setupChildView(View view) {
+        validateChildView();
+        childViews.add(view);
+        if (childViews.size() == 1) {
+            addCircularRevealView();
+        }
     }
-  }
 
-  private void validateFAB() {
-    if(fab != null){
-      throw new IllegalArgumentException("FABRevealLayout can only hold one FloatingActionButton");
+    private void validateFAB() {
+        if (fab != null) {
+            throw new IllegalArgumentException("FABRevealLayout can only hold one FloatingActionButton");
+        }
     }
-  }
 
-  private void validateChildView() {
-    if(childViews.size() >= MAX_CHILD_VIEWS){
-      throw new IllegalArgumentException("FABRevealLayout can only hold two views");
+    private void validateChildView() {
+        if (childViews.size() >= MAX_CHILD_VIEWS) {
+            throw new IllegalArgumentException("FABRevealLayout can only hold two views");
+        }
     }
-  }
 
-  private void addCircularRevealView() {
-    circularExpandingView = new CircularExpandingView(getContext());
-    LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-    params.topMargin = dipsToPixels(FAB_SIZE);
-    circularExpandingView.setVisibility(View.GONE);
-    addView(circularExpandingView, params);
-  }
-
-  private boolean areAllComponentsReady(){
-    return fab != null && childViews.size() == MAX_CHILD_VIEWS;
-  }
-
-  private void setupInitialState(){
-    setupFABPosition();
-    setupChildViewsPosition();
-  }
-
-  private void setupFABPosition(){
-    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) fab.getLayoutParams();
-    params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
-    params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      params.rightMargin = dipsToPixels(16);
-      params.topMargin = dipsToPixels(20);
+    private void addCircularRevealView() {
+        circularExpandingView = new CircularExpandingView(getContext());
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        params.topMargin = dipsToPixels(FAB_SIZE);
+        circularExpandingView.setVisibility(View.GONE);
+        addView(circularExpandingView, params);
     }
-    fab.bringToFront();
-  }
 
-  private void setupChildViewsPosition(){
-    for(int i = 0; i < childViews.size(); i++){
-      RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) childViews.get(i).getLayoutParams();
-      params.topMargin = dipsToPixels(FAB_SIZE);
+    private boolean areAllComponentsReady() {
+        return fab != null && childViews.size() == MAX_CHILD_VIEWS;
     }
-    getSecondaryView().setVisibility(GONE);
-  }
 
-  private boolean isShowingMainView(){
-    return getMainView().getVisibility() == VISIBLE;
-  }
-
-  public void revealMainView(){
-    if(!isShowingMainView()){
-      if (isAnimating.compareAndSet(false, true)) {
-        startHideAnimation();
-      }
+    private void setupInitialState() {
+        setupFABPosition();
+        setupChildViewsPosition();
     }
-  }
 
-  public void revealSecondaryView(){
-    if(isShowingMainView()){
-      if (isAnimating.compareAndSet(false, true)) {
-        startRevealAnimation();
-      }
+    private void setupFABPosition() {
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) fab.getLayoutParams();
+        params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+        params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            params.rightMargin = dipsToPixels(16);
+            params.topMargin = dipsToPixels(20);
+        }
+        fab.bringToFront();
     }
-  }
 
-  public void setOnRevealChangeListener(OnRevealChangeListener onRevealChangeListener) {
-    this.onRevealChangeListener = onRevealChangeListener;
-  }
+    private void setupChildViewsPosition() {
+        for (int i = 0; i < childViews.size(); i++) {
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) childViews.get(i).getLayoutParams();
+            params.topMargin = dipsToPixels(FAB_SIZE);
+        }
+        getSecondaryView().setVisibility(GONE);
+    }
 
-  private void startRevealAnimation(){
-    View disappearingView = getMainView();
+    private boolean isShowingMainView() {
+        return getMainView().getVisibility() == VISIBLE;
+    }
 
-    ObjectAnimator fabAnimator = getFABAnimator();
-    ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(disappearingView, "alpha", 1, 0);
+    public void revealMainView() {
+        if (!isShowingMainView()) {
+            if (isAnimating.compareAndSet(false, true)) {
+                startHideAnimation();
+            }
+        }
+    }
 
-    AnimatorSet set = new AnimatorSet();
-    set.play(fabAnimator).with(alphaAnimator);
-    setupAnimationParams(set);
-    set.addListener(new AnimatorListenerAdapter() {
-      @Override
-      public void onAnimationEnd(Animator animation) {
-        super.onAnimationEnd(animation);
-        fab.setVisibility(GONE);
-        prepareForReveal();
-        expandCircle();
-        isAnimating.set(false);
-      }
-    } );
+    public void revealSecondaryView() {
+        if (isShowingMainView()) {
+            if (isAnimating.compareAndSet(false, true)) {
+                startRevealAnimation();
+            }
+        }
+    }
 
-    set.start();
-  }
+    @SuppressWarnings("unused")
+    public void setOnRevealChangeListener(OnRevealChangeListener onRevealChangeListener) {
+        this.onRevealChangeListener = onRevealChangeListener;
+    }
 
-  private void prepareForReveal() {
-    circularExpandingView.getLayoutParams().height = getMainView().getHeight();
-    circularExpandingView.setColor(fab.getBackgroundTintList() != null ?
-        fab.getBackgroundTintList().getDefaultColor() :
-        0xFF000000
+    private void startRevealAnimation() {
+        View disappearingView = getMainView();
+
+        ObjectAnimator fabAnimator = getFABAnimator();
+        ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(disappearingView, "alpha", 1, 0);
+
+        AnimatorSet set = new AnimatorSet();
+        set.play(fabAnimator).with(alphaAnimator);
+        setupAnimationParams(set);
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                fab.setVisibility(GONE);
+                prepareForReveal();
+                expandCircle();
+            }
+        });
+
+        set.start();
+    }
+
+    private void prepareForReveal() {
+        circularExpandingView.getLayoutParams().height = getMainView().getHeight();
+        circularExpandingView.setColor(fab.getBackgroundTintList() != null ?
+                fab.getBackgroundTintList().getDefaultColor() :
+                0xFF000000
         );
-    circularExpandingView.setVisibility(VISIBLE);
-  }
-
-  private void setupAnimationParams(Animator animator) {
-    animator.setInterpolator(INTERPOLATOR);
-    animator.setDuration(ANIMATION_DURATION);
-  }
-
-  private CurvedAnimator getCurvedAnimator() {
-    View view = getMainView();
-
-    float fromX = fab.getLeft();
-    float fromY = fab.getTop();
-    float toX = view.getWidth() / 2 - fab.getWidth() / 2 + view.getLeft();
-    float toY = view.getHeight() / 2 - fab.getHeight() / 2 + view.getTop();
-
-    if(isShowingMainView()) {
-      return new CurvedAnimator(fromX, fromY, toX, toY);
-    }else{
-      return new CurvedAnimator(toX, toY, fromX, fromY);
+        circularExpandingView.setVisibility(VISIBLE);
     }
-  }
 
-  private ObjectAnimator getFABAnimator(){
-    CurvedAnimator curvedAnimator = getCurvedAnimator();
-    return ObjectAnimator.ofObject(this, "fabPosition", new CurvedPathEvaluator(), curvedAnimator.getPoints());
-  }
-
-  private void expandCircle(){
-    Animator expandAnimator = circularExpandingView.expand();;
-    expandAnimator.addListener(new AnimatorListenerAdapter() {
-      @Override
-      public void onAnimationEnd(Animator animation) {
-        super.onAnimationEnd(animation);
-        swapViews();
-      }
-    });
-    expandAnimator.start();
-  }
-
-  private void startHideAnimation(){
-    Animator contractAnimator = circularExpandingView.contract();
-    View disappearingView = getSecondaryView();
-    ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(disappearingView, "alpha", 1, 0);
-
-    AnimatorSet set = new AnimatorSet();
-    set.play(contractAnimator).with(alphaAnimator);
-    setupAnimationParams(set);
-    set.addListener(new AnimatorListenerAdapter() {
-      @Override
-      public void onAnimationEnd(Animator animation) {
-        super.onAnimationEnd(animation);
-        fab.setVisibility(VISIBLE);
-        circularExpandingView.setVisibility(GONE);
-        moveFABToOriginalLocation();
-        isAnimating.set(false);
-      }
-    });
-    set.start();
-  }
-
-  private void moveFABToOriginalLocation(){
-    ObjectAnimator fabAnimator = getFABAnimator();
-
-    setupAnimationParams(fabAnimator);
-    fabAnimator.addListener(new AnimatorListenerAdapter() {
-      @Override
-      public void onAnimationEnd(Animator animation) {
-        super.onAnimationEnd(animation);
-        swapViews();
-      }
-    });
-
-    fabAnimator.start();
-  }
-
-  public void setFabPosition(Point point){
-    fab.setX(point.x);
-    fab.setY(point.y);
-  }
-
-  private void swapViews() {
-    if(isShowingMainView()){
-      getMainView().setVisibility(GONE);
-      getMainView().setAlpha(1);
-      getSecondaryView().setVisibility(VISIBLE);
-      circularExpandingView.setVisibility(VISIBLE);
-    }else{
-      getMainView().setVisibility(VISIBLE);
-      getSecondaryView().setVisibility(GONE);
-      getSecondaryView().setAlpha(1);
-      circularExpandingView.setVisibility(View.GONE);
+    private void setupAnimationParams(Animator animator) {
+        animator.setInterpolator(INTERPOLATOR);
+        animator.setDuration(ANIMATION_DURATION);
     }
-    notifyListener();
-  }
 
-  private void notifyListener(){
-    if(onRevealChangeListener != null){
-      if(isShowingMainView()){
-        onRevealChangeListener.onMainViewAppeared(this, getMainView());
-      }else{
-        onRevealChangeListener.onSecondaryViewAppeared(this, getSecondaryView());
-      }
+    private CurvedAnimator getCurvedAnimator() {
+        View view = getMainView();
+
+        float fromX = fab.getLeft();
+        float fromY = fab.getTop();
+        float toX = view.getWidth() / 2 - fab.getWidth() / 2 + view.getLeft();
+        float toY = view.getHeight() / 2 - fab.getHeight() / 2 + view.getTop();
+
+        if (isShowingMainView()) {
+            return new CurvedAnimator(fromX, fromY, toX, toY);
+        } else {
+            return new CurvedAnimator(toX, toY, fromX, fromY);
+        }
     }
-  }
 
-  private View getSecondaryView() {
-    return childViews.get(1);
-  }
+    private ObjectAnimator getFABAnimator() {
 
-  private View getMainView() {
-    return childViews.get(0);
-  }
+        CurvedAnimator curvedAnimator = getCurvedAnimator();
+        return ObjectAnimator.ofObject(this, "fabPosition", new CurvedPathEvaluator(), curvedAnimator.getPoints());
+    }
 
-  private int dipsToPixels(float dips){
-    return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dips, getResources().getDisplayMetrics());
-  }
+    private void expandCircle() {
+        Animator expandAnimator = circularExpandingView.expand();
 
-  @Override
-  public void setLayoutParams(ViewGroup.LayoutParams params) {
-    ((MarginLayoutParams) params).topMargin -= dipsToPixels(FAB_SIZE);
-    super.setLayoutParams(params);
-  }
+        expandAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                swapViews();
+                isAnimating.set(false);
+            }
+        });
+        expandAnimator.start();
+    }
 
+    private void startHideAnimation() {
+        Animator contractAnimator = circularExpandingView.contract();
+        View disappearingView = getSecondaryView();
+        ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(disappearingView, "alpha", 1, 0);
+
+        AnimatorSet set = new AnimatorSet();
+        set.play(contractAnimator).with(alphaAnimator);
+        setupAnimationParams(set);
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                fab.setVisibility(VISIBLE);
+                circularExpandingView.setVisibility(GONE);
+                moveFABToOriginalLocation();
+            }
+        });
+        set.start();
+    }
+
+    private void moveFABToOriginalLocation() {
+        ObjectAnimator fabAnimator = getFABAnimator();
+
+        setupAnimationParams(fabAnimator);
+        fabAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                swapViews();
+                isAnimating.set(false);
+            }
+        });
+
+        fabAnimator.start();
+    }
+
+    @SuppressWarnings("unused")
+    public void setFabPosition(SimplePoint point) {
+        fab.setX(point.x);
+        fab.setY(point.y);
+
+
+    }
+
+    private void swapViews() {
+        if (isShowingMainView()) {
+            getMainView().setVisibility(GONE);
+            getMainView().setAlpha(1);
+            getSecondaryView().setVisibility(VISIBLE);
+            circularExpandingView.setVisibility(VISIBLE);
+        } else {
+            getMainView().setVisibility(VISIBLE);
+            getSecondaryView().setVisibility(GONE);
+            getSecondaryView().setAlpha(1);
+            circularExpandingView.setVisibility(View.GONE);
+        }
+        notifyListener();
+    }
+
+    private void notifyListener() {
+        if (onRevealChangeListener != null) {
+            if (isShowingMainView()) {
+                onRevealChangeListener.onMainViewAppeared(this, getMainView());
+            } else {
+                onRevealChangeListener.onSecondaryViewAppeared(this, getSecondaryView());
+            }
+        }
+    }
+
+    private View getSecondaryView() {
+        return childViews.get(1);
+    }
+
+    private View getMainView() {
+        return childViews.get(0);
+    }
+
+    private int dipsToPixels(float dips) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dips, getResources().getDisplayMetrics());
+    }
+
+    @Override
+    public void setLayoutParams(ViewGroup.LayoutParams params) {
+        ((MarginLayoutParams) params).topMargin -= dipsToPixels(FAB_SIZE);
+        super.setLayoutParams(params);
+    }
+
+
+    public interface OnRevealChangeListener {
+        void onMainViewAppeared(FABRevealLayout layout, View mainView);
+
+        void onSecondaryViewAppeared(FABRevealLayout layout, View secondaryView);
+
+    }
 }
